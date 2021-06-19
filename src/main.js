401270ed818149b9f17a449724c50a7f07f1794e
Ryan Boldi <ryan.boldi123@gmail.com>
2021-06-19 14:23:50 +0400

    added atoms up to 30

--- src/main.js
+++ src/main.js
@@ -11,8 +11,9 @@ function setup(){
         line(0, 400, 800, 400);
     }
 
-    let DISPLAY_ATOMS = true;
+    let DISPLAY_ATOMS = false;
     if(DISPLAY_ATOMS){
+        let a;
         for (let i = 100; i < 800; i += 100){
             a = new Atom(i/100);
             a.drawAt(i, 100);
@@ -24,6 +25,10 @@ function setup(){
         for (let i = 100; i < 800; i += 100){
             a = new Atom(i/100 + 14);
             a.drawAt(i, 300);
+        }
+        for (let i = 100; i < 800; i += 100){
+            a = new Atom(i/100 + 21);
+            a.drawAt(i, 400);
         }
     }
 }
